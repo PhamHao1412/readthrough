12de98fd15a0a986e3fc2c93ce1f4df951a115a5
Hao Pham <hao.pham@kyanon.digital>
2026-06-08 11:55:01 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,7 @@ echo "=== Starting readthrough ==="
 # Run migrations if DB.URL is set
 if [ -n "$DB_URL" ]; then
     echo ">>> Running database migrations..."
-    
-    # Prepare connection URL containing search_path for goose
-    MIGRATE_URL="$DB_URL"
-    if echo "$MIGRATE_URL" | grep -Fq "?"; then
-        # If query parameters already exist, append using &
-        if ! echo "$MIGRATE_URL" | grep -Fq "search_path="; then
-            MIGRATE_URL="${MIGRATE_URL}&search_path=readful"
-        fi
-    else
-        # If no query parameters exist, append using ?
-        MIGRATE_URL="${MIGRATE_URL}?search_path=readful"
-    fi
-
-    # Run goose migrations in the readful schema
-    goose -dir /app/data/migrations postgres "$MIGRATE_URL" up
+    goose -dir /app/data/migrations postgres "$DB_URL" up
     echo ">>> Migrations done!"
 else
     echo ">>> WARNING: DB_URL not set, skipping migrations"
